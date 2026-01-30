79fece47138710b42c9895bf1deb54436e77ed97
Murray Collier <murraycollier@gmail.com>
2026-01-30 14:35:30 +0000

    Update bootstrap script to install Starship
    Added installation of Starship prompt to bootstrap script.

--- bootstrapcs.sh
+++ bootstrapcs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz &&
-  sudo rm -rf /opt/nvim-linux-x86_64 &&
-  sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz &&
-  echo 'eval "$(starship init bash)"' >>~/.bashrc &&
-  echo 'export PATH=/opt/nvim-linux-x86_64/bin/:$PATH' >>~/.bashrc
+sudo rm -rf /opt/nvim-linux-x86_64 &&
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz &&
+curl -sS https://starship.rs/install.sh | sh
+echo 'eval "$(starship init bash)"' >>~/.bashrc &&
+echo 'export PATH=/opt/nvim-linux-x86_64/bin/:$PATH' >>~/.bashrc
