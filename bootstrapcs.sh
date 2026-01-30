b99b9c25d9fd1cf0027e7d3f4eef2d5a8eb7d895
Murray Collier <murraycollier@gmail.com>
2026-01-30 15:02:54 +0000

    Install LazyVim and update bash configuration
    Added LazyVim starter configuration and updated bashrc.

--- bootstrapcs.sh
+++ bootstrapcs.sh
@@ -3,7 +3,8 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz &&
 sudo rm -rf /opt/nvim-linux-x86_64 &&
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz &&
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | sh &&
+git clone https://github.com/LazyVim/starter ~/.config/nvim &&
 echo 'eval "$(starship init bash)"' >>~/.bashrc &&
 echo 'export PATH=/opt/nvim-linux-x86_64/bin/:$PATH' >>~/.bashrc &&
 cat <<'EOF' > ~/.config/starship.toml
